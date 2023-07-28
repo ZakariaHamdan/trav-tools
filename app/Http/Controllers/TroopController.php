3bf88197c5d841e6d5ef1831098a1725ae490d5e
= <=>
2023-07-28 13:54:10 +0300

    inital

---
+++ app/Http/Controllers/TroopController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Troop;
+use Illuminate\Http\Request;
+
+class TroopController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Troop $troop)
+    {
+        //
+    }
+    
+    
+    public function edit(Troop $troop)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, Troop $troop)
+    {
+        //
+    }
+
+    
+    public function destroy(Troop $troop)
+    {
+        //
+    }
+}

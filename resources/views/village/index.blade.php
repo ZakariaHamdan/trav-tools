b68ef56f2d7d1512f06c6fddb62a9688df2f7d04
= <=>
2023-08-31 16:33:10 +0300

    a

--- resources/views/village/index.blade.php
+++ resources/views/village/index.blade.php
@@ -6,6 +6,6 @@
     <div class="container">
         <a href="{{ route('villages.create') }}" class="btn btn-success mb-3">Create</a>
         <x-index-table :items=$villages prefix="villages"
-                       columns="name,x,y,lumber,clay,iron,crop"/>
+                       columns="name,x,y,lumber,clay,iron,net_lumber,net_clay,net_iron"/>
     </div>
 @endsection
